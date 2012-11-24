de761a4f9e1b35c2f2e6ce1d78f0a45b53ed1480
Dan Ackroyd <Danack@basereality.com>
2012-11-24 23:30:51 +1100

    Working version.
    Everything _seems_ to be working. Which seems a bit suspicious.

--- phar2/index2.php
+++ phar2/index2.php
@@ -1,24 +1,55 @@
 <?php
 
+/**
+ * Sanity test that the phar1 file is readable inside the phar2 file
+ */
+function testPhar(){
+	$fileHandle = fopen("phar://phar2/phar1.phar", "r");
+	$result = fread($fileHandle, 512);
+	echo "Read result is [".$result."]\r\n";
+	fclose($fileHandle);
+}
 
 
-//Phar::loadPhar("phar://phar2.phar/phar1.phar", "wibble");
+//This just doesn't work.
+//Phar::loadPhar("phar://phar2/phar1.phar", "phar1");
+
+/**
+ * Extract phar1 to an external file and then load it.
+ */
+function extractAndLoadPhar(){
+
+	$tempFilename =  tempnam( sys_get_temp_dir() , "phar");
+	//$tempFilename =  "/tmp/phar1.phar";
+
+	//echo "tempFilename is $tempFilename\r\n";
+
+	$readHandle = fopen("phar://phar2/phar1.phar", "r");
+	$writeHandle =  fopen($tempFilename, "w");
+
+	while (!feof($readHandle)) {
+		$result = fread($readHandle, 512);
+		fwrite($writeHandle, $result);
+	}
+
+	fclose($readHandle);
+	fclose($writeHandle);
+
+	$result = Phar::loadPhar($tempFilename, "phar1");
+	//echo "Load result is $result \r\n";
+}
 
 
-$fileHandle = fopen("phar://phar2/phar1.phar", "r");
 
-
-$result = fread($fileHandle, 512);
-
-echo "Read result is [".$result."]\r\n";
-
-fclose($fileHandle);
-
-//include ('phar://phar2/phar1.phar/phar1/src1.php');
 include ('phar://phar2/src2.php');
-
 test2();
 
+extractAndLoadPhar();
+
+//include ('phar://phar1/index1.php');
+include ('phar://phar1/src1.php');
 test1();
+
+exit(0);
 
 ?>
---
+++ readme.txt
@@ -0,0 +1,7 @@
+
+
+1) Create the phars with 'sh create.sh'
+
+2) Run them with 'sh run.sh'
+
+
